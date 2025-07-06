b8dbf7e35b1d5d832f0ce48c7de81e82eca589d3
mactave2000 <matheisclement@gmail.com>
2025-07-07 00:01:41 +0200

    Add files via upload

---
+++ games.ts
@@ -0,0 +1,16 @@
+export const games = [
+  {
+    id: 1,
+    title: "GTA V",
+    image: "/images/gta5.jpg",
+    price: 29.99,
+    category: "payant",
+  },
+  {
+    id: 2,
+    title: "2048",
+    image: "/images/2048.jpg",
+    price: 0,
+    category: "gratuit",
+  },
+];
