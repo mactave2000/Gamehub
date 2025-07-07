c5acb2c8e2817cf585ee801fd32052d509ddc847
mactave2000 <matheisclement@gmail.com>
2025-07-07 09:39:07 +0200

    Add files via upload

--- app/Profil/page.tsx
+++ app/Profil/page.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Page = () => {
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-2xl font-bold">Bienvenue sur la page : profil</h1>
+      <p>Contenu à venir...</p>
+    </div>
+  );
+};
+
+export default Page;
