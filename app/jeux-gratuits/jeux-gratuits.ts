839f57c85ab69ffe9e99900df43671c24369d707
mactave2000 <matheisclement@gmail.com>
2025-07-07 09:54:37 +0200

    Add files via upload

---
+++ app/jeux-gratuits/jeux-gratuits.ts
@@ -0,0 +1,42 @@
+export const jeuxGratuits = [
+  {
+    id: 1,
+    title: "2048",
+    image: "/images/2048.jpg",
+    url: "https://play2048.co/",
+    device: "Mobile & PC",
+    noDownload: true,
+  },
+  {
+    id: 2,
+    title: "Slither.io",
+    image: "/images/slitherio.jpg",
+    url: "https://slither.io/",
+    device: "Mobile & PC",
+    noDownload: true,
+  },
+  {
+    id: 3,
+    title: "Tetris",
+    image: "/images/tetris.jpg",
+    url: "https://tetris.com/play-tetris",
+    device: "PC",
+    noDownload: true,
+  },
+  {
+    id: 4,
+    title: "Krunker.io",
+    image: "/images/krunker.jpg",
+    url: "https://krunker.io/",
+    device: "PC",
+    noDownload: true,
+  },
+  {
+    id: 5,
+    title: "Geoguessr (Free Mode)",
+    image: "/images/geoguessr.jpg",
+    url: "https://www.geoguessr.com/free",
+    device: "Mobile & PC",
+    noDownload: true,
+  }
+];
